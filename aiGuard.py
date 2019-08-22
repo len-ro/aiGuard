a09a3eeec7f2aaaa1fcf4561c7992fac8b99c636
Len Corciovei <len@len.ro>
2019-08-22 13:00:00 +0300

    working version

--- aiGuard.py
+++ aiGuard.py
@@ -3,11 +3,16 @@
 import sys
 import time
 import json
-import os, signal
+import os, signal, re
 import logging, logging.config
+import threading
 
 from watchdog.observers import Observer
 from watchdog.events import RegexMatchingEventHandler
+from imageai.Detection import ObjectDetection
+from queue import Queue
+
+from modules.processor import Processor
 
 class SignalCatcher:
     kill_now = False
@@ -19,70 +24,132 @@ class SignalCatcher:
     def exit_gracefully(self, signum, frame):
         self.kill_now = True
 
-class DirectoryMonitor(RegexMatchingEventHandler):
-    """ Single directory monitor """ 
-    def __init__(self, name, config):
-        self.name = name
-        self.config = config
-        if 'regexp' in self.config:
-            super().__init__(self.config['regexp'])
+
+class DirEventHandler(RegexMatchingEventHandler):
+    """ Single directory event handler """ 
+    def __init__(self, config, process_function):
+        self.logger = logging.getLogger("dirEventHandler")
+        self.process_function = process_function
+        if 'regexp' in config:
+            super().__init__([config['regexp']])
         else:
             super().__init__()
-        self.__event_handler = self
-        self.__event_observer = Observer()
-        self.logger = logging.getLogger("directoryMonitor")
 
     def on_created(self, event):
         """ wait for file to be complete, not perfect """
+        self.logger.info("New file: " + event.src_path)
         file_size = -1
         while file_size != os.path.getsize(event.src_path):
             file_size = os.path.getsize(event.src_path)
             time.sleep(1)
-        self.process(event)
-
-    def start(self):
-        if not os.path.isdir(self.config['path']):
-            self.logger.error("Not a directory: %s" % self.config['path'])
-            self.started = False
-            return
-        self.__event_observer.schedule(
-            self.__event_handler,
-            self.config['path'],
-            recursive=False
-        )
-        self.__event_observer.start()
-        self.started = True
-        self.logger.info("start monitoring %s" % self.config['path'])
-
-    def stop(self):
-        if self.started:
-            self.__event_observer.stop()
-            self.__event_observer.join()
-            self.logger.info("stop monitoring %s" % self.config['path'])
-
-
-    def process(self, event):
-        self.logger.info("Processing new file %s" % event.src_path)
+        self.process_function(event.src_path)        
 
 class aiGuard:
     def __init__(self):
         self.config = json.load(open('config.json', 'r'))
         self.setup_logging()
-        self.monitors = []
+        self.observer = Observer()
+        self.queue = Queue(10)
+        
+        self.processors = {}
+        for p_name in self.config['processors']:
+            self.processors[p_name] = Processor(self.config['processors'][p_name])
 
     def setup_logging(self):
         logging.config.dictConfig(self.config['logging'])
         self.logger = logging.getLogger('aiGuard')
 
+    def mk_subdir(self, base_dir, name):
+        """ creates a new dir in base_dir if it does not exist """
+        new_dir = os.path.join(base_dir, name)
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+        return new_dir
+
+    def detector_thread(self, config):
+        """Image detection in a single thread"""
+        detector = ObjectDetection()
+
+        if config['type'] == 'YOLOv3':
+            detector.setModelTypeAsYOLOv3()
+        elif self.config['type'] == 'RetinaNet':
+            detector.setModelTypeAsRetinaNet()
+
+        detector.setModelPath(config['model'])
+        detector.loadModel()
+
+        self.logger.info("Model loaded")
+
+        while True:
+            input_path = self.queue.get()
+            if input_path == False: #termination condition
+                self.queue.task_done()
+                break
+            if os.path.isfile(input_path):
+                dirname, basename = os.path.split(input_path)
+                output_image = os.path.join(self.mk_subdir(dirname, config['outdir']), basename)
+                self.logger.info("Analysing new file %s -> %s" % (input_path, output_image))
+                detections = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_image)
+                #process file
+                if dirname in self.processors:
+                    processor = self.processors[dirname]
+                    try:
+                        processor.process(input_path, output_image, detections)
+                    except:
+                        self.logger.error("Unexpected error in process", exc_info=1)
+                else:
+                    self.logger.warn("No processor found for %s in %s" % (dirname, self.processors.keys())) 
+            self.queue.task_done()
+
+    def process_file(self, file_path):
+        """
+        In response to a file event, send the file for processing to the detector_thread
+        see: https://github.com/OlafenwaMoses/ImageAI/issues/125 why a single thread is needed
+        """
+        self.queue.put(file_path)
+
     def monitor(self):
+        """This is the main entry point"""
         self.logger.info('aiGuard start')
         killer = SignalCatcher()
 
+        """ init detector thread """
+        self.detector = threading.Thread(target=self.detector_thread, args=(self.config['detector'],))
+        self.detector.start()
+        self.logger.info("Detector thread started")
+
         """ init monitors """
         for dir in self.config['directories']:
-            monitor = DirectoryMonitor(dir, self.config['directories'][dir])
-            monitor.start()
-            self.monitors.append(monitor)
+            dir_config = self.config['directories'][dir]
+            if not os.path.isdir(dir_config['path']):
+                self.logger.error("Not a directory: %s" % dir_config['path'])
+            else:
+                dir_path = dir_config['path']
+
+                processor_name = dir_config['processor']
+                if processor_name in self.processors:
+                    self.processors[dir_path] = self.processors[processor_name]
+                else:
+                    self.logger.warn('No processor %s defined' % processor_name)
+                    #TODO error
+
+                if dir_config.get('processFirst', 'true') == 'true':
+                    #we will process all files in this dir first
+                    list = os.listdir(dir_path)
+                    file_re = re.compile(dir_config['regexp'])
+                    for file_name in list:
+                        file_path = os.path.join(dir_path, file_name)
+                        if os.path.isfile(file_path) and file_re.match(file_name):
+                            self.process_file(file_path)
+
+                self.logger.info("start monitoring %s" % dir_path)
+                self.observer.schedule(
+                    DirEventHandler(dir_config, self.process_file),
+                    dir_config['path'],
+                    recursive=False
+                )
+
+        self.observer.start()
 
         """main loop"""
         while True:
@@ -91,8 +158,15 @@ class aiGuard:
 
             time.sleep(self.config['timeout'])
 
-        for m in self.monitors:
-            m.stop()
+        self.logger.info("aiGuard stopping")
+
+        self.queue.put(False)
+
+        self.observer.stop()
+        self.observer.join()
+        
+        self.queue.join()
+        self.detector.join()
         self.logger.info('aiGuard stop')
 
 if __name__ == "__main__":
---
+++ modules/processor.py
@@ -0,0 +1,109 @@
+import logging, os
+import piexif, piexif.helper, json
+import numpy as np
+from pushover import Client
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+            np.int16, np.int32, np.int64, np.uint8,
+            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32, 
+            np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+class Processor():
+    def __init__(self, config):
+        self.logger = logging.getLogger("processor")
+        self.config = config
+        self.pushover = None
+        if 'actions' in config:
+            for a in config['actions']:
+                if a == 'pushover':
+                    pushover_config = config['actions'][a]
+                    if pushover_config['active'] == 'true':
+                        self.pushover = Client(pushover_config['user-key'], api_token=pushover_config['api-token'])
+
+
+    def move_to_dir(self, file_path, dir_path):
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        new_path = os.path.join(dir_path, os.path.basename(file_path))
+        os.rename(file_path, new_path)
+
+    def store_detections_in_exif(self, files, detections):
+        """Stores detections map in exif for later usage"""
+        detections_as_json = json.dumps(detections, indent=4, cls=NumpyEncoder)
+        exif_dict = {
+            "Exif": {
+                piexif.ExifIFD.UserComment: piexif.helper.UserComment.dump(detections_as_json)
+                }
+            }
+        exif_bytes = piexif.dump(exif_dict)
+        for file_path in files:
+            piexif.insert(exif_bytes, file_path)
+
+    def process_features(self, input_image, output_image, subdir, subdir_src, detections, action, action_message = None):
+        self.store_detections_in_exif([input_image, output_image], detections)
+        
+        try:
+            if action == 'pushover' and self.pushover:
+                if not action_message:
+                    action_message = 'Feature detected in %s!' % input_image
+                with open(output_image, 'rb') as image:
+                    self.pushover.send_message(action_message, attachment=image)
+        except:
+            self.logger.error("Unexpected error in action", exc_info=1)
+            
+        self.move_to_dir(output_image, subdir)
+        self.move_to_dir(input_image, subdir_src)
+
+    def process(self, input_image, output_image, detections):
+        for c in self.config['classes']:
+            self.logger.info("Processing class %s" % c)
+            d = c['detections']
+            #output_image is already in processed
+            subdir = os.path.join(os.path.dirname(output_image), c['name'])
+            subdir_src = os.path.join(subdir, 'src')
+            action = c.get('action', None)
+
+            #nothing found
+            if d['mode'] == 'none' and not detections:
+                self.logger.info("[none] Nothing found in %s, moving to %s" % (input_image, subdir))
+                self.move_to_dir(input_image, subdir)
+                os.remove(output_image)
+                return
+
+            if d['mode'] == 'any':
+                for feature in detections:
+                    f_name = feature['name']
+                    if f_name in d['keys']:
+                        self.logger.info("[any] Feature %s found in %s, moving to %s" % (f_name, input_image, subdir))
+                        self.process_features(input_image, output_image, subdir, subdir_src, detections, action)
+                        return
+
+            if d['mode'] == 'all':
+                has_all_features = True
+                for feature in detections:
+                    f_name = feature['name']
+                    if f_name not in d['keys']:
+                        has_all_features = False
+                if has_all_features:
+                    self.logger.info("[all] features found in %s, moving to %s" % (input_image, subdir))
+                    self.process_features(input_image, output_image, subdir, subdir_src, detections, action)
+                    return
+            
+            if d['mode'] == 'copy':
+                self.logger.info("[move] %s, moving to %s" % (input_image, subdir))
+                self.process_features(input_image, output_image, subdir, subdir_src, detections, action)
+                return                
+
+
+            
+            
+            
