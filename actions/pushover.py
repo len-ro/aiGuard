0afacbab6508b6c2d89865ff0f39cc6b58c7b784
Len Corciovei <len@len.ro>
2019-08-27 22:35:27 +0300

    pushover fix

--- actions/pushover.py
+++ actions/pushover.py
@@ -14,18 +14,18 @@ class Pushover:
         self.config['lock-file'] = os.path.join(os.getcwd(), self.config['lock-file'])
 
     def action(self, message, image_file):
-        if self.config.active == 'true':
+        if self.config['active'] == 'true':
             t = int(time.time())
             delta = t - self.last_time
             self.last_time = t
-            if os.path.exists(self.config['lock-file']):
-                if delta > self.config.timeout:
+            if not os.path.exists(self.config['lock-file']):
+                if delta > self.config['timeout']:
                     if not message:
                         message = 'Feature detected in %s!' % image_file
                     with open(image_file, 'rb') as image:
                         self.logger.info("Sending pushover notification %s %s" % (message, image_file))
                         self.pushover_api.send_message(message, attachment=image)
                 else:
-                    self.logger.info("Not sending pushover notification %s %s, timeout window is %s of %s" % (message, image_file, delta, self.config.timeout))
+                    self.logger.info("Not sending pushover notification %s %s, timeout window is %s of %s" % (message, image_file, delta, self.config['timeout']))
             else:
                 self.logger.info("Not sending pushover notification %s %s, lock file exists %s!" % (message, image_file, self.config['lock-file']))
